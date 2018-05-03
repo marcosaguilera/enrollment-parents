c0e9a913a7a7f127b63da306d0674b72a013644c
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-05-03 16:53:54 -0500

    Creating components

---
+++ src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './global/css/App.css';
+
+//Components declaration
+import Header from './global/Header'
+import Content from './global/Content'
+import Footer from './global/Footer'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+          <Header />
+          <Content />
+          <Footer />
+     
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/global/Content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './css/Content.css';
+
+class Content extends Component {
+  render() {
+    return (
+      <div className="Content">
+        Wow, it's just a Content
+      </div>
+    );
+  }
+}
+
+export default Content;
