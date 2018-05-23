3d3f5b1678c486d7d8fe918fdb078f42a1339614
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-05-23 12:49:20 -0500

    Resume with Fee

--- src/components/global/Modules/Resume/Resume.js
+++ src/components/global/Modules/Resume/Resume.js
@@ -8,6 +8,8 @@ class Resume extends Component {
   constructor(){
     super();
 
+    this.handleCalculateTotalPayFee    = this.handleCalculateTotalPayFee.bind(this);
+
     this.state = {
       seguro        : 0,
       anuario       : 0,
@@ -16,8 +18,9 @@ class Resume extends Component {
       tot_matricula : 0,
       tot_servicios : 0,
       tot_pagar     : 0,
-      fee           : 3.2,
+      fee           : 2.9,
       fee_cop       : 900,
+      tot_tarifa    : 0,
       codigo        : '',
       nombres       : '',
       apellidos     : '',
@@ -43,13 +46,19 @@ class Resume extends Component {
         nombres       : servicesObj.nombres,
         apellidos     : servicesObj.apellidos,
         grado         : servicesObj.grado,
-        objectId      : servicesObj.uid
+        objectId      : servicesObj.uid,
+        
 
     }, () => {
-        console.log("Updated value: " + this.state.seguro);
+        console.log("Calculating fee value");
+        this.handleCalculateTotalPayFee();
     });
-    
+  }
 
+  handleCalculateTotalPayFee = () =>{
+    this.setState({
+      tot_tarifa    : Number((this.state.tot_pagar * this.state.fee)/100)+this.state.fee_cop
+    })
   }
 
   // Props definitions
@@ -133,11 +142,35 @@ class Resume extends Component {
                                     <td>Afiliación Club Deportivo</td>
                                     <td><NumberFormat value={this.state.club} displayType={'text'} thousandSeparator={true} prefix={'$'} /></td>
                                   </tr>
+                                  <tr className="table-primary">
+                                    <td ><b>Total Matrícula + Servicios</b></td>
+                                    <td><b><NumberFormat value={this.state.tot_pagar} displayType={'text'} thousandSeparator={true} prefix={'$'} /></b></td>
+                                  </tr>
+                                  <tr className="table-warning">
+                                    <td>Tarifa pasarela ePayCo <br/> 2,99% + 900$</td>
+                                    <td><NumberFormat value={this.state.tot_tarifa} displayType={'text'} thousandSeparator={true} prefix={'$'} /></td>
+                                  </tr>
                                 </tbody>
                               </table>
                             </div>
                           </div>
                       </div>
+
+                      <div className="py-3">
+                        <div className="col-md-12">
+                          <div class="row">
+                            <div class="col">
+                                <button type="button" class="btn btn-primary btn-lg">Imprimir</button>
+                            </div>
+                            <div class="col"></div>
+                            <div class="col"></div>
+                            <div class="col">
+                                <button type="button" class="btn btn-primary btn-lg">Pagar en línea</button>
+                            </div>
+                          </div>      
+                        </div>
+                      </div>
+
                     </div>
                   </div>
                 </div>
