96a0e3ab934a36e859c6896e6525925da10e78fe
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2018-05-24 14:08:13 -0500

    Print

--- src/components/global/Modules/Print/Print.js
+++ src/components/global/Modules/Print/Print.js
@@ -1,20 +1,43 @@
 // Dependencies
 import React, { Component } from 'react';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
+
+//Assets
+import './Print.css';
 
 class Print extends Component {
-  
-  constructor(){
-    super(); 
+  constructor(props) {
+    super(props);
   }
 
-  
-  render() {   
+  printDocument() {
+    const input = document.getElementById('divToPrint');
+    html2canvas(input)
+      .then((canvas) => {
+          const imgData = canvas.toDataURL('image/png');
+          const pdf = new jsPDF();
+
+          pdf.addImage(imgData, 'JPEG', 0, 0);
+          
+          // pdf.output('dataurlnewwindow');
+          pdf.save("download.pdf");
+      });
+  }
+
+  render() {
     return (
       <div className="Print">
-        Hello print!
+        <div className="mb5">
+            <button onClick={this.printDocument}>Print</button>
+        </div>
+        <div id="divToPrint">
+          <div>Note: Here the dimensions of div are same as A4</div> 
+          <div>You Can add any component here</div>
+        </div>
       </div>
     );
   }
 }
 
-export default Print; 
+export default Print;
