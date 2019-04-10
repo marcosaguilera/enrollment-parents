17bc94f837fa4ec1a0ebd9303dcf85deb7561e0c
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2019-04-10 08:41:55 -0500

    new route

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,8 @@ import Resume from './components/global/Modules/Resume/Resume'
 import Search from './components/global/Modules/Search/Search'
 import Help from './components/global/Modules/Help/Help'
 import Print from './components/global/Modules/Print/Print'
-import HomeOldStudents from './components/global/Modules/Services/Services'
+import EnrolmentAnnualServices from './components/global/Modules/Services/Services'
+import EnrolmentMontlyServices from './components/global/Modules/ServicesMontly/ServicesMontly'
 import HomeNewStudents from './components/global/Modules/NewStudents/NewStudents'
 import Home from './components/global/Modules/Home/Home'
 import Page404 from './components/global/Modules/Page404/Page404'
@@ -21,7 +22,8 @@ const AppRoutes = () =>
             <Route exact path="/help" component={Help} />
             <Route exact path="/print" component={Print} />
             <Route exact path="/home" component={Home} />
-            <Route exact path="/enrollment_old_students" component={HomeOldStudents} />
+            <Route exact path="/enrolment_annual_services" component={EnrolmentAnnualServices} />
+            <Route exact path="/enrolment_montly_services" component={EnrolmentMontlyServices} />
             <Route exact path="/enrollment_new_students" component={HomeNewStudents} />
             <Route exact component={Page404} />
         </Switch>
