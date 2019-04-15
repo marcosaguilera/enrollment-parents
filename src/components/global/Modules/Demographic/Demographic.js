22376490e9ade2173a0b2ee42f7af9ce1b0d3c74
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2019-04-15 10:37:57 -0500

    Demographic component updated

--- src/components/global/Modules/Demographic/Demographic.js
+++ src/components/global/Modules/Demographic/Demographic.js
@@ -1,33 +1,36 @@
 import React, { Component } from 'react';
 import store from '../../../../ReduxStore/store'
 
+import './Demographic.css'
+
 class Demographic extends Component {
 
     constructor() {
         super();
         this.state = {
-            student_demographic_data : {}
+            demo_data         : [],
+            hello_fake        : '',
+            student_code      : '',
+            student_grade     : '',
+            student_name      : '',
+            student_lastname  : ''
         };
 
         store.subscribe(() => {
             this.setState({
-                student_demographic_data : store.getState().essential_data
+              hello_fake       : store.getState().fake_text,
+              demo_data        : store.getState().demo_data,
+              student_code     : store.getState().demo_data.custom_id,
+              student_grade    : store.getState().demo_data.grade,
+              student_name     : store.getState().demo_data.first_name,
+              student_lastname : store.getState().demo_data.last_name,
             }, () => {
-              console.log(this.state.student_demographic_data)
             })
         })
     }
 
-    componentWillMount(){
-        console.log(this.state.student_demographic_data)
-    }
-
-    componentWillUpdate(){
-        console.log(this.state.student_demographic_data)
-    }
-
     componentWillReceiveProps(){
-        console.log("hello")
+        //console.log("hello")
     }
 
     render() {
@@ -38,22 +41,22 @@ class Demographic extends Component {
                       <h4 className="d-flex justify-content-between mb-3" >Información del estudiante</h4>
                       <div className="row">
                         <div className="col-md-6 mb-3">
-                        <label htmlFor="firstName">Código</label>
-                        <input type="text" className="form-control onlyReadInput" id="Code" value={this.state.codigo} readOnly="readonly"></input>
+                        <label className="customLabel" htmlFor="firstName">Código</label>
+                        <input type="text" className="onlyReadInput" id="Code" value={this.state.student_code} readOnly="readonly"></input>
                       </div>
                       <div className="col-md-6 mb-3">
-                        <label htmlFor="firstName">Grado</label>
-                        <input type="text" className="form-control onlyReadInput" id="Grade" value={this.state.grado} readOnly="readonly"></input>
+                        <label className="customLabel" htmlFor="firstName">Grado</label>
+                        <input type="text" className="onlyReadInput" id="Grade" value={this.state.student_grade} readOnly="readonly"></input>
                       </div>
                     </div>
                     <div className="row">
                       <div className="col-md-6 mb-3">
-                        <label htmlFor="firstName">Nombres</label>
-                        <input type="text" className="form-control onlyReadInput" id="firstName" value={this.state.nombres} readOnly="readonly"></input>
+                        <label className="customLabel" htmlFor="firstName">Nombres</label>
+                        <input type="text" className="onlyReadInput" id="firstName" value={this.state.student_name} readOnly="readonly"></input>
                       </div>
                       <div className="col-md-6 mb-3">
-                        <label htmlFor="lastName">Apellidos</label>
-                        <input type="text" className="form-control onlyReadInput" id="lastName" value={this.state.apellidos} readOnly="readonly"></input>
+                        <label className="customLabel" htmlFor="lastName">Apellidos</label>
+                        <input type="text" className="onlyReadInput" id="lastName" value={this.state.student_lastname} readOnly="readonly"></input>
                       </div>
                     </div>
                   </div>
