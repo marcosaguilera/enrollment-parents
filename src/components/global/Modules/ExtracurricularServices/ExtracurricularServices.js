b3f06b292485bc201fdd4fc043ad636828049bd2
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2019-05-06 17:19:11 -0500

    New UI elementss added
    Cart list elements added

--- src/components/global/Modules/ExtracurricularServices/ExtracurricularServices.js
+++ src/components/global/Modules/ExtracurricularServices/ExtracurricularServices.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+
+// Depencies
+import axios from 'axios';
 import PropTypes from 'prop-types';
 import NumberFormat from 'react-number-format';
 import store from '../../../../ReduxStore/store'
+import {ToastsContainer, ToastsStore} from 'react-toasts';
 
 import { Button, Card } from 'react-bootstrap';
 
@@ -29,6 +33,8 @@ class ExtracurricularServices extends Component {
     constructor(props) {
         super(props);
 
+        this.addEcoService = this.addEcoService.bind(this);
+
         this.state = {
             code     : '',
             name     : '',
@@ -45,9 +51,12 @@ class ExtracurricularServices extends Component {
                 { id: 8, name: "On Motion Live", description: "These quality short-sleeve crew-neck t-shirts are 100% pre-shrunk cotton. Fit is unisex standard (size up in doubt). S, M, L, XL, XLT, 2XL, 2XLT, 3XL, 3XLT available.", price: 99, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-2.jpg" },
                 { id: 9, name: "Underground Max", description: "These quality short-sleeve crew-neck t-shirts are 100% pre-shrunk cotton. Fit is unisex standard (size up in doubt). S, M, L, XL, XLT, 2XL, 2XLT, 3XL, 3XLT available.", price: 149, image: "https://s3.amazonaws.com/makeitreal/projects/e-commerce/camiseta-3.jpg" },
             ],
-            ecoServices: [],
+            services: [],
+            cartServices : [],
             step3_data: {},
-            isReadyDemographicComponent : false
+            isReadyDemographicComponent : false,
+            showingAlert: false
+
         }
     }
 
@@ -62,8 +71,46 @@ class ExtracurricularServices extends Component {
             lastname   : servicesObj.demographic.apellidos,
             grade      : servicesObj.demographic.grado,
             step3_data : servicesObj,
-        }, () => { this.setState({ isReadyDemographicComponent : true }) })
+        }, () => {
+            this.setState({ isReadyDemographicComponent : true })
+            this.loadEcoServices(this.state.grade)
+        })
     }
+
+    loadEcoServices(grade){
+        let url = "https://rcis-backend.herokuapp.com/student/ecoservices/" + grade
+        axios.get(url)
+            .then(res => {
+                console.log(res.data)
+                this.setState({ services : res.data })
+            })
+    }
+
+    addEcoService(data){
+        let cartArray = this.state.cartServices
+        if(cartArray.length < 2){
+            cartArray.push(data)
+            console.log(cartArray)
+            this.setState({ cartServices : cartArray }, () => { console.log(this.state.cartServices.length) })
+        }else{
+            console.log("No puedes agregar mas")
+            //alert("No puedes agregar mas actividades.")
+            ToastsStore.warning("No puedes agregar mas actividades.")
+            //this.handleClickShowAlert()
+        }
+    }
+
+    handleClickShowAlert() {
+        this.setState({
+          showingAlert: true
+        });
+
+        setTimeout(() => {
+          this.setState({
+            showingAlert: false
+          });
+        }, 2000);
+      }
 
     render() {
         return (
@@ -71,37 +118,41 @@ class ExtracurricularServices extends Component {
              <main role="main"  className="container" id="customStyle">
                 <div className="p-3 mb-5 bg-white rounded">
 
-                    if(this.state.isReadyDemographicComponent){
-                       <Demographic code={"--"+this.state.code}
-                       grade={this.state.grade}
-                       name={this.state.name}
-                       lastname={this.state.lastname} />
+                    {this.state.isReadyDemographicComponent}{
+                       <Demographic code={this.state.code}
+                            grade={this.state.grade}
+                            name={this.state.name}
+                            lastname={this.state.lastname} />
                     }
 
                     <hr/>
                     <div className="row">
                         <div className="col-md-8">
                             <div style={styles.products}>
-                                {this.state.products.map(product =>
-                                    <div className="card cardCustom" key={product.id}>
-                                        <img src={product.image} className="card-img-top cardImgCustom" alt="Product image" />
+                                {this.state.services.map(service =>
+                                    <div className="card cardCustom" key={service.id}>
+                                        {/*<img src={service.image} className="card-img-top cardImgCustom" alt="Service image" />*/}
                                         <div className="card-body">
-                                            <h5 className="card-title cardTitleCustom">{product.name}</h5>
-                                            <p className="card-text cardDescriptionTextCustom">{product.description}</p>
+                                            <h5 className="card-title cardTitleCustom">{service.name}</h5>
+                                            <p className="card-text cardDescriptionTextCustom">{service.description}</p>
                                         </div>
                                         <div className="card-footer">
                                             <div id="boxContainer">
                                                 <div id="box1">
-                                                    <a id="addActivityBtn" href="#" className="btn btn-primary">Inscribir</a>
+                                                    <button
+                                                           id="addActivityBtn"
+                                                           className="btn btn-primary"
+                                                           onClick={ () => this.addEcoService(service) }>Inscribir</button>
                                                 </div>
                                                 <div id="box2">
-                                                    <NumberFormat id="priceSpan" value={product.price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
+                                                    <NumberFormat id="priceSpan" value={service.value} displayType={'text'} thousandSeparator={true} prefix={'$'} />
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 )}
                             </div>
+                            <ToastsContainer store={ToastsStore} timer={5000}/>
                         </div>
                         <div className="col-md-4">
                             <div className="card">
@@ -110,11 +161,9 @@ class ExtracurricularServices extends Component {
                                 </div>
                                 <div className="card-body">
                                     <ul className="list-group">
-                                        <li className="list-group-item">Cras justo odio</li>
-                                        <li className="list-group-item">Dapibus ac facilisis in</li>
-                                        <li className="list-group-item">Morbi leo risus</li>
-                                        <li className="list-group-item">Porta ac consectetur ac</li>
-                                        <li className="list-group-item">Vestibulum at eros</li>
+                                        {this.state.cartServices.map(cart =>
+                                            <li className="list-group-item" key={cart.id}> {cart.name} </li>
+                                        )}
                                     </ul>
                                 </div>
                                 <div className="card-footer bg-success text-white">
