3ab3eb7b8867a2659d58d1e58873ead38d24df35
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2019-05-16 12:44:25 -0500

    New functions and codes
    I've added new functions and services codes

--- src/Utils/Utils.js
+++ src/Utils/Utils.js
@@ -1,6 +1,8 @@
 let getServiceCode =  function(serviceName){
     //////// Annual services
-    if(serviceName === 'Matricula'){ return '0001' }
+    if(serviceName === 'Matricula Plena'){ return '0001' }
+    if(serviceName === 'Matricula 50'){ return '0003' }
+    if(serviceName === 'Matricula 100'){ return '0002' }
     if(serviceName === 'Bibliobanco'){ return '0005' }
     if(serviceName === 'Seguro accidentes'){ return '0008' }
     if(serviceName === 'Asopadres'){ return '0010' }
@@ -10,13 +12,20 @@ let getServiceCode =  function(serviceName){
     if(serviceName === 'Impreso y digital - $156.000'){ return 'XXXX' }
 
     //////// Montly services
-    if(serviceName === 'Pension'){ return 'P9001' }
-    if(serviceName === 'Transporte'){ return 'T9002' }
-    if(serviceName === 'Almuerzo'){ return 'A9003' }
-    if(serviceName === 'Medias Nueves'){ return 'M9004' }
-    if(serviceName === 'Desayuno'){ return 'D9005' }
-    if(serviceName === 'Seguro de vida'){ return 'S9006' }
-    if(serviceName === 'Seguro desempleo'){ return 'S9007' }
+    if(serviceName === 'Pension Plena'){ return '0020' }
+    if(serviceName === 'Pension 50'){ return '0022' }
+    if(serviceName === 'Pension 100'){ return '0021' }
+    if(serviceName === 'Transporte Completo Cercano'){ return '0026' }
+    if(serviceName === 'Transporte Completo Intermedio'){ return '0027' }
+    if(serviceName === 'Transporte Completo Lejano'){ return '0028' }
+    if(serviceName === 'Transporte Medio Lejano'){ return '0031' }
+    if(serviceName === 'Transporte Medio Intermedio'){ return '0030' }
+    if(serviceName === 'Transporte Medio Cercano'){ return '0029' }
+    if(serviceName === 'Almuerzo'){ return '0024' }
+    if(serviceName === 'Medias Nueves'){ return '0025' }
+    if(serviceName === 'Desayuno'){ return '0098' }
+    if(serviceName === 'Seguro de vida'){ return '0032' }
+    if(serviceName === 'Seguro desempleo'){ return '0092' }
     if(serviceName === 'Otro'){ return 'O9008' }
 }
 
@@ -45,9 +54,29 @@ let checkSelection = function(value){
     else{ return 'No' }
 }
 
+let checkNull = function(value){
+    return value === null ? 0 : value
+}
+
+let getMatriculaName = function(tarifa_plena, tarifa_reducida_7_5, tarifa_reducida_15){
+    if(tarifa_plena > 0){ return 'Matricula Plena' }
+    if(tarifa_reducida_7_5 > 0){ return 'Matricula 50' }
+    if(tarifa_reducida_15 > 0){ return 'Matricula 100' }
+}
+
+let getPensionName = function(matriculaCode){
+    console.log(matriculaCode)
+    if(matriculaCode === "0001"){ return 'Pension Plena' }
+    if(matriculaCode === "0002"){ return 'Pension 100' }
+    if(matriculaCode === "0003"){ return 'Pension 50' }
+}
+
 module.exports = {
     getServiceCode,
     getTransportServiceName,
     existTextMatch,
-    checkSelection
+    checkSelection,
+    checkNull,
+    getMatriculaName,
+    getPensionName
 }
