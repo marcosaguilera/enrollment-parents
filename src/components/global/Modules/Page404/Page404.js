87d24da4d6a16cf5cdc5c13c0dfa8eed54d1bb01
Marcos Antonio Aguilera Ely <marcosaguileraely@gmail.com>
2019-05-29 15:38:40 -0500

    Redirecto to home

--- src/components/global/Modules/Page404/Page404.js
+++ src/components/global/Modules/Page404/Page404.js
@@ -2,6 +2,11 @@
 import React, { Component } from 'react';
 
 class Page404 extends Component {
+
+  componentDidMount(){
+    this.props.history.push('/home')
+  }
+
   render() {
     return (
       <div className="Page404">
